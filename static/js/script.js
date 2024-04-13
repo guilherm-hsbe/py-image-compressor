3a7cb2d7f216e4a7d70783633874a4e2291d6d63
guilherm-hsbe <guilherm3hsb@gmail.com>
2024-04-13 19:40:35 -0300

    feat(script): Inclui funções de controle do botão de upload

--- static/js/script.js
+++ static/js/script.js
@@ -18,3 +18,21 @@ window.onclick = function(event) {
     if (event.target == license)
         handleLicenseVisibility('hidden');
 }
+
+/** Set loading icons to file upload */
+function handleFileUploadIcon() {
+    let iconUpload = document.getElementById('icnUpload');
+    iconUpload.classList.remove('fa', 'fa-cloud-upload');
+    iconUpload.classList.add('fa-solid', 'fa-spinner', 'fa-spin');
+}
+
+/** Disabled the file input and set loading icon when file is detected */
+function manageFileInput() {
+    if (this.files && this.files[0]) {
+        let file = this.files[0];
+        this.disabled = true;
+        handleFileUploadIcon();
+    }
+}
+let fileUpload = document.getElementById('flUpload');
+fileUpload.addEventListener('change', manageFileInput);
