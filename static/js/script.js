61aece2bed0a6a0cc8775cb3fa71fdb2a9c0c222
guilherm-hsbe <guilherm3hsb@gmail.com>
2024-04-13 14:03:35 -0300

    feat(script): Inclui funções JS para controle de visibilidade do modal de licença

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,18 @@ function closeAlert() {
     let alert = document.getElementById('pnlAlert');
     alert.style.visibility = 'hidden'
 }
+
+/** Controls license modal visibility
+ * @param {string} visibility
+ */
+function handleLicenseVisibility(visibility) {
+    let license = document.getElementById('pnlLicense');
+    license.style.visibility = visibility;
+}
+
+/** Hide license modal when outside click event triggers */
+window.onclick = function(event) {
+    let license = document.getElementById('pnlLicense');
+    if (event.target == license)
+        handleLicenseVisibility('hidden');
+}
